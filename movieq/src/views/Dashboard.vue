82f4c78d068fbef14caa748b2fdd72edb91435da
Ikhraaazh <ikhsanrafi06@gmail.com>
2022-09-14 13:48:41 +0800

    login feature done

---
+++ movieq/src/views/Dashboard.vue
@@ -0,0 +1,14 @@
+<template>
+  <sidebar/>
+  <router-view/>
+</template>
+
+<script>
+  import sidebar from '../components/Sidebar.vue';
+
+  export default {
+    components: {
+      sidebar
+    }
+  }
+</script>
