b7b9107f2d3a4903daee22fd5fda67c003f6f6f6
isoletslicer <ardogible@gmail.com>
2022-09-14 13:59:12 +0700

    Rev : integrating user profile page with server data ,minus card

--- client/src/views/UserProfilePage.vue
+++ client/src/views/UserProfilePage.vue
@@ -1,33 +1,54 @@
-<script></script>
+<script>
+import { mapActions, mapWritableState } from "pinia";
+import { useUserProfileMethodStore } from "../stores/userProfileMethod";
+
+export default {
+  methods: {
+    ...mapActions(useUserProfileMethodStore, ["getUserProfile"]),
+  },
+  computed: {
+    ...mapWritableState(useUserProfileMethodStore, [
+      "username",
+      "bio",
+      "totalSpellCard",
+      "totalMonsterCard",
+      "totalTrapCard",
+      "totalWin",
+      "totalLose",
+      "cardList",
+    ]),
+  },
+
+  created() {
+    this.getUserProfile();
+  },
+};
+</script>
 
 <template>
   <div class="container mt-4 mb-4 p-3 d-flex justify-content-center">
     <div class="card p-4">
       <div class="d-flex flex-column justify-content-center align-items-center">
-        <span class="name mt-3">Eleanor Pena</span>
-        <span class="idd">@eleanorpena</span>
-        <div
-          class="d-flex flex-row justify-content-center align-items-center gap-2"
-        >
-          <span class="idd1">Oxc4c16a645_b21a</span>
-          <span><i class="fa fa-copy"></i></span>
-        </div>
-        <div
-          class="d-flex flex-row justify-content-center align-items-center mt-3"
-        >
-          <span class="number">1069 <span class="follow">Followers</span></span>
-        </div>
+        <span class="name mt-3">{{ username }}</span>
         <div class="d-flex mt-2">
           <button class="btn1 btn-dark">Edit Profile</button>
         </div>
         <div class="text mt-3">
-          <span
-            >Eleanor Pena is a creator of minimalistic x bold graphics and
-            digital artwork.<br /><br />
-            Artist/ Creative Director by Day #NFT minting@ with FND night.
-          </span>
+          <span> {{ bio }} </span>
         </div>
 
+        <div class="text px-2 rounded mt-4">
+          Statistics :
+          <div class="container">
+            Cards Gained : <br />
+            Monster Cards : {{ totalMonsterCard }} <br />
+            Trap Cards :
+            {{ totalTrapCard }} <br />
+            Spell Cards : {{ totalSpellCard }}
+          </div>
+        </div>
+
+        <div class="px-2 rounded mt-4">Latest 3 cards :</div>
         <div class="px-2 rounded mt-4 date">
           <span class="join">Joined May,2021</span>
         </div>
