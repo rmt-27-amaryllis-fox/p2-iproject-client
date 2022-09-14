91346ccb88d5fb75c6283cddbfb0fe894ddeb705
William <mauritius.william.wijaya1@gmail.com>
2022-09-14 23:40:49 +0700

    google-sign-in done

--- client/src/App.vue
+++ client/src/App.vue
@@ -13,12 +13,57 @@ export default {
       "fetchMyOrder",
       "handleCredentialResponse",
     ]),
-    
+    handleCredentialResponse(response) {
+      console.log("Encoded JWT ID token: " + response.credential);
+      axios({
+        method: "POST",
+        url: this.baseUrl + "/google-sign-in",
+        headers: {
+          token_dari_google: response.credential,
+        },
+        data: {
+          token_dari_google: response.credential,
+        },
+      })
+        .then((response) => {
+          localStorage.setItem("access_token", response.data[0].access_token);
+          localStorage.setItem("email", response.data[1].email);
+          localStorage.setItem("authorId", response.data[1].id);
+          this.fetchIphone();
+          this.fetchMyOrder();
+          this.changePage('')
+          if (localStorage.getItem("access_token")) {
+            this.isLogin = true;
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          Swal.fire({
+            title: `ERROR`,
+            text: `${err.responseJSON}`,
+            icon: "error",
+            confirmButtonText: "Try Again",
+          });
+        });
+    },
   },
   computed: {
-    ...mapWritableState(appleStore, ["isLogin", 'baseUrl']),
+    ...mapWritableState(appleStore, ["isLogin", "baseUrl"]),
   },
-  
+  mounted() {
+    const callback = this.handleCredentialResponse;
+    window.onload = function () {
+      google.accounts.id.initialize({
+        client_id:
+          "814776840113-mflcqpso4ddfsf9qubf2qvp803paq7a7.apps.googleusercontent.com",
+        callback,
+      });
+      google.accounts.id.renderButton(document.getElementById("buttonDiv"), {
+        theme: "outline",
+        size: "large",
+      });
+    };
+  },
   created() {
     this.fetchIphone();
     if (localStorage.getItem("access_token")) {
