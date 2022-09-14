0cfad5ed7ad8c7e32ab2155b832752481f657534
isoletslicer <ardogible@gmail.com>
2022-09-15 06:20:03 +0700

    Rev: revamping landing page + pecah komponent konten

---
+++ client/src/components/ParagraphOpening.vue
@@ -0,0 +1,19 @@
+<script></script>
+
+<template>
+  <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+    <p class="fs-5 mb-4">
+      Play with Simple YGO TCG Way !
+      <br />
+      No more Complicated Methods!
+      <br />
+      Just like other Randomized play !
+      <br />
+      Simplicity 3vs3 cards !
+      <br />
+      Compare your total atk!
+    </p>
+  </div>
+</template>
+
+<style></style>
