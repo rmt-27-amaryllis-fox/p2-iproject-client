7b78f7e735854ec940feed6ed2b9c681eb5a1002
isan <ihellostereo@gmail.com>
2022-09-15 10:39:55 +0700

    Update: minor addjustment and add chat box with socketIO

--- RISETARNISH/src/App.vue
+++ RISETARNISH/src/App.vue
@@ -31,7 +31,7 @@ export default {
 </script>
 
 <template>
-  <!-- <ChatAppVue /> -->
+  <ChatAppVue />
 
   <RouterView />
 </template>
--- RISETARNISH/src/components/ChatApp.vue
+++ RISETARNISH/src/components/ChatApp.vue
@@ -8,35 +8,19 @@ export default {
   components: { },
   data() { 
     return {
+      display: false,
       text: '',
-      messages: [
-        {
-          id: 1,
-          text: 'test',
-          user: 'testUser'
-        }
-      ]
+      messages: []
     }
   },
   computed: {
     ...mapState(useUserStore, {
-      username: 'username'
+      username: 'username',
+      isLogin: 'isLogin'
     })
   },
   methods: {
-    join() {
-      console.log(this.currentUser)
-      this.joinend = true
-      this.socketInstance = io('http://localhost:3000', {
-        transports: ['websocket']
-      })
-
-      this.socketInstance.on('message:received', (data) => {
-        this.messages = this.messages.concat(data)
-      })
-    },
     sendMessage() {
-      console.log(this.text)
       this.addMessage()
       this.text = ''
     },
@@ -49,29 +33,144 @@ export default {
 
       this.messages = this.messages.concat(message)
       
-      this.socketInstance.emit('message', message)
+      // this.socketInstance.emit('message', message)
+    },
+    openChat() {
+      this.display = true
+    },
+    closeChat() {
+      this.display = false
     }
   },
-  created() { },
+  created() {
+    this.text = ''
+  },
 }
 </script>
 
 <template>
-  <div>
-    <div>
-      <div v-for="message in messages" :key="message.id">
-        <b>
-          {{ message.user }}
-        </b>
-        : {{ message.text }}
+  <button v-if="!display" class="open-button" @click.prevet="openChat">Chat</button>
+  
+  <div v-if="display" class="chat-popup" id="myForm">
+    <form class="form-container">
+      <p>RISE TARNISHED</p>
+
+      <label for="msg"><b>Message</b></label>
+      <div class="grid-one-by-one">
+        <div class="chat-boxes overflow-auto">
+          <div v-for="message in messages" :key="message.id">
+            <b>
+              {{ message.user }}
+            </b>
+            : {{ message.text }}
+          </div>
+        </div>
+        <div v-if="isLogin" class="text-warper">
+          <textarea 
+            placeholder="Type message.."
+            name="msg"
+            @keyup.enter="sendMessage"
+            v-model="text"
+          >
+          </textarea>
+        </div>
       </div>
-    </div>
-    <div class="text-input-container">
-      <textarea v-model="text" @keyup.enter="sendMessage" class="text-message"></textarea>
-    </div>
+      <div class="row button-warper">
+        <div v-if="isLogin" class="col">
+          <button type="submit" class="btn btn-outline-secondary" @click.prevent="sendMessage">Send</button>
+        </div>
+        <div v-if="!isLogin">
+          <p class="small" style="color: red;">Please Login First</p>
+        </div>
+        <div class="col flex-item">
+          <button type="button" class="btn cancel" @click.prevent="closeChat">Close</button>
+        </div>
+      </div>
+    </form>
   </div>
 </template>
 
 <style scoped>
+body {font-family: Arial, Helvetica, sans-serif;}
+* {box-sizing: border-box;}
+
+/* Button used to open the chat form - fixed at the bottom of the page */
+.open-button {
+  background-color: #555;
+  color: white;
+  padding: 16px 20px;
+  border: none;
+  cursor: pointer;
+  opacity: 0.8;
+  position: fixed;
+  bottom: 0px;
+  left: 0px;
+}
+
+div.chat-boxes{
+  overflow: scroll;
+}
+
+div.grid-one-by-one {
+  display: grid;
+  grid-template-rows: 200px 50px;
+}
+
+.chat-popup {
+  width: 17em;
+  height: 500px;
+  position: fixed;
+  bottom: 2em;
+  left: 0.5em;
+  border: 3px solid #f1f1f1;
+  z-index: 9;
+}
+
+.form-container {
+  max-width: 300px;
+  padding: 10px;
+  background-color: white;
+}
+
+.form-container textarea {
+  width: 100%;
+  padding: 10px;
+  margin: 5px 0 22px 0 15px;
+  border: none;
+  background: #f1f1f1;
+  resize: none;
+}
+
+.form-container textarea:focus {
+  background-color: #ddd;
+  outline: none;
+}
+
+.form-container .btn {
+  background-color: rgb(155, 121, 47);
+  color: white;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+  margin-bottom:10px;
+  opacity: 0.8;
+}
+
+.form-container .cancel {
+  background-color: rgb(177, 49, 49);
+}
+
+.form-container .btn:hover, .open-button:hover {
+  opacity: 1;
+}
+
+div.button-warper {
+  margin-top: 1.5em;
+}
+div.flex-item {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
   
